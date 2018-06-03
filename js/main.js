9f9ac3e6360dfc213e3387037aa8774511407cae
karolkalkus <karolkalkus6@gmail.com>
2018-06-03 20:31:13 +0200

    small changes

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*jslint browser: true*/
+/*global $*/
+$('a[href^="#"]').on('click', function (event) {
+
+      var target = $(this.getAttribute ('href'));
+
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    });
